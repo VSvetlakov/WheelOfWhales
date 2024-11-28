a20bf77fd5f1fe436ad5adb07f47fa4e9eab2b84
yummy1gay <131353317+yummy1gay@users.noreply.github.com>
2024-11-29 00:36:30 +0100

    .

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -3,7 +3,6 @@ headers = {
     'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Origin': 'https://clicker.crashgame247.io',
-    'Priority': 'u=1, i',
     'Referer': 'https://clicker.crashgame247.io/',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
